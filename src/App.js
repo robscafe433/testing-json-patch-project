4ede00770d20192cc9070293e802bb8cea05a10d
Robert S <robscafe333@yahoo.com>
2022-05-04 20:45:14 -0700

    testing the json patch method

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useFetch from "./api/store";
+
+function clickButton() {
+  fetch("http://localhost:3000/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      author: "Darwin",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
 
 function App() {
+  const storeData = useFetch("http://localhost:3000/posts");
+  console.log("TypeOf : ", typeof storeData);
+  var rows = [];
+
+  if (storeData != null) {
+    storeData.forEach((element) => {
+      rows.push(
+        <tr key={element.id}>
+          <td>{element.id}</td>
+          <td>{element.title}</td>
+          <td>{element.author}</td>
+        </tr>
+      );
+    });
+  }
+
   return (
     <div className="App">
+      {console.log(storeData)}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Hello How are you?</div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+        <div>
+          <br />
+          <br />
+          <button onClick={clickButton}>
+            Click to update part of the data
+          </button>
+        </div>
       </header>
     </div>
   );
---
+++ src/api/store.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json);
+    }
+
+    fetchData();
+  }, [url]);
+  
+
+  return data;
+};
+
+export default useFetch;
